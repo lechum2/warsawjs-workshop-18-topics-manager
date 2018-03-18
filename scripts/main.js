fe57fe4cd9112321cb886e1131ecc8ee4a21e26b
Sebastian Lech <lechum2@gmail.com>
2018-03-18 13:51:44 +0100

    correct comment

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@ require('bulma');
 
 const $loginButton = document.querySelector(".js-login-button-github");
 $loginButton.addEventListener("click", (event) => {
-    //event.preventDefault(); //if link should prevent page reload
+    //event.preventDefault(); //if link, this should prevent page reload
     console.log('login to github clicked')
 });
