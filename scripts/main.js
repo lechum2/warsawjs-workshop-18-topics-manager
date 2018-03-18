c2069bb7244c09718a0d3f3a105f92d8c1cf56db
Sebastian Lech <lechum2@gmail.com>
2018-03-18 15:40:03 +0100

    Add user avatar and login after login

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,19 @@ $loginButton.addEventListener("click", event => {
     //event.preventDefault(); //if link, this should prevent page reload
     hello('github').login()
         .then(() => {return hello('github').api('/me')})
-        .then(userProfile => console.log(userProfile));
+        .then(userProfile => renderUserDetails(userProfile));
 });
+
+
+
+function renderUserDetails(userProfile) {
+    const template = `
+        <div class="navbar-item">
+            ${ userProfile.login }
+            <img src="${userProfile.avatar_url}" alt="${userProfile.name}"/>
+        </div>
+        `
+    const $navbar = document.querySelector(".js-userpanel");
+    $navbar.innerHTML += template;
+    //console.log($navbar);
+}
